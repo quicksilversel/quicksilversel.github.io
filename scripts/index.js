3eb1da35e7938ae3f640966e4b980f5ee610f72b
quicksilversel <quicksilversel@gmail.com>
2021-01-22 02:03:22 +0900

    added project 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-  if ($(window).width() > 992) {
+  if ($(window).width() > 992) 
+
+  // fixed navigation bar on scroll 
+  {
     $(window).scroll(function(){  
        if ($(this).scrollTop() > 40) {
-          $('.navbar').addClass("fixed-top");
+          $(".navbar").addClass("fixed-top");
           // add padding top to show content behind navbar
-          $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+          $("body").css("padding-top", $(".navbar").outerHeight() + "px");
         }
         else{
-          $('.navbar').removeClass("fixed-top");
+          $(".navbar").removeClass("fixed-top");
            // remove padding top from body
-          $('body').css('padding-top', '0');
+          $("body").css("padding-top", "0");
         }   
     });
   } 
-      
+
+  // change active list on click
+  $("ul li a").click(function(){ 
+    $("li a").removeClass("active"); 
+    $(this).addClass("active"); 
+  });
 });
