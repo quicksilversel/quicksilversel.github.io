f83c599af68249b71dcd206ff175dc061d2bcfa7
quicksilversel <quicksilversel@gmail.com>
2021-01-31 14:28:26 +0900

    updated design

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,27 @@ $(document).ready(function() {
   // fixed navigation bar on scroll 
   {
     $(window).scroll(function(){  
-       if ($(this).scrollTop() > 40) {
+      var topValue = document.getElementById("portfolio").offsetTop
+       if ($(this).scrollTop() > topValue) {
           $(".navbar").addClass("fixed-top");
           // add padding top to show content behind navbar
-          $("body").css("padding-top", $(".navbar").outerHeight() + "px");
+          $("#porfolio").css("padding-top", $(".navbar").outerHeight() + "px");
         }
         else{
           $(".navbar").removeClass("fixed-top");
            // remove padding top from body
-          $("body").css("padding-top", "0");
+          $("#portfolio").css("padding-top", "0");
         }   
     });
   } 
+  
 
   // change active list on click
   $("ul li a").click(function(){ 
     $("li a").removeClass("active"); 
     $(this).addClass("active"); 
   });
+
+
 
 });
