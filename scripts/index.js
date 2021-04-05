389df5002c545c75bce5603204c2a545bd2fb897
quicksilversel <quicksilversel@gmail.com>
2021-04-05 09:31:06 +0900

    loading screen fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // loader 
   setTimeout(function() {
     $('.loading-container').fadeOut('fast');
-  }, 1500);
+  }, 4000);
 
   // fade into view
   $(window).scroll(function() {
